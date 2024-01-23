e0e5874eff4484178f25de9431bef266fcf3406b
InesaFitsner <inesa@appveyor.com>
2024-01-23 11:59:10 -0800

    Pagelet control docs
    bottom_appbar property for page
    trailing property for CupertinoAppBar

--- sidebars.js
+++ sidebars.js
@@ -105,6 +105,7 @@ module.exports = {
             "controls/listtile",
             "controls/listview",
             "controls/page",
+            "controls/pagelet",
             "controls/responsiverow",
             "controls/row",
             "controls/safearea",
